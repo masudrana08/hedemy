c7590ee1eec92fce321a3e1d41d171372f57719a
masudrana08 <rana028511@gmail.com>
2020-08-27 02:24:56 +0600

    courses component design part completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CourseData from "./CourseData";
+import Courses from "./Components/Courses/Courses";
+import Cart from "./Components/Cart/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-info">
+
+      <div className="header">
+        <h1 className="header-title">hello world</h1>
+      </div>
+
+      <div className="course-container">
+        <div className="course-details ">
+          {CourseData.map((course) => {
+            return <Courses key={course.id} course={course} />;
+          })}
+        </div>
+
+        <div className="cart">
+          <Cart />
+        </div>
+      </div>
+
     </div>
   );
 }
