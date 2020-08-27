414a26fd0514a8aee5fd9082310334053099598f
masudrana08 <rana028511@gmail.com>
2020-08-27 18:16:38 +0600

    hedemy assignment done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import CourseData from "./CourseData";
 import Courses from "./Components/Courses/Courses";
--- src/CourseData.js
+++ src/CourseData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 const CourseData=[
     {
         id:1,
